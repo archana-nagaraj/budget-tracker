8b3e8d17884e30a1d9fcc797469d854edd257ecc
Archana Nagaraja <johndoe@example.com>
2021-04-25 10:41:56 -0700

    craete /establish db connection

---
+++ public/js/idb.js
@@ -0,0 +1,26 @@
+// create variable to hold db connection
+let db;
+// establish a connection to IndexedDB database 
+const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+// upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store 
+    db = event.target.result;
+    // check if app is online
+    if (navigator.onLine) {
+      uploadTransaction();
+    }
+  };
+  
+request.onerror = function(event) {
+console.log(event.target.errorCode);
+};
+
+
