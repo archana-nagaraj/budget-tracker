48dc3005b843115dacedd743ff3fb074d56076fd
Archana Nagaraja <johndoe@example.com>
2021-04-25 15:22:16 -0700

    code changes for debugging

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,65 +1,74 @@
-const APP_PREFIX = "BudgetTracker-";
-const VERSION = "version_01";
-const DATA_CACHE_NAME = 'data-cache-' + VERSION;
+const APP_PREFIX = 'budget-';
+const VERSION = 'v_03';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "/",
     "./index.html",
-    "./css/styles.css",
-    "./js/idb.js",
+    "./assets/css/styles.css",
     "./js/index.js",
+    "./js/idb.js",
     "./manifest.json",
-    "./icons/icon-72x72.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png"
-  ];
+    "./assets/icons/icon-72x72.png",
+    "./assets/icons/icon-96x96.png",
+    "./assets/icons/icon-128x128.png",
+    "./assets/icons/icon-144x144.png",
+    "./assets/icons/icon-152x152.png",
+    "./assets/icons/icon-192x192.png",
+    "./assets/icons/icon-384x384.png",
+    "./assets/icons/icon-512x512.png"
+];
+
+
 
 // Cache resources
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("installing cache : " + CACHE_NAME);
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener('install', function (evt) {
+  evt.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (evt) {
+  evt.waitUntil(
+        caches.keys().then(function (keyList) {
+            // `keyList` contains all cache names under your username.github.io
+            // filter out ones that has this app prefix to create keeplist
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            // add current cache name to keeplist
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
 });
 
 // Respond with cached resources
-self.addEventListener("fetch", function (e) {
-  if (e.request.url.includes("/api/")) {
-    e.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(e.request)
-            .then((response) => {
-              if (response.status === 200) {
-                cache.put(e.request.url, response.clone());
-              }
-              return response;
-            })
-            .catch((err) => {
-              return cache.match(e.request);
-            });
-        })
-        .catch((err) => console.log(err))
-    );
-    return;
-  }
-  e.respondWith(
-    fetch(e.request).catch(function () {
-      return caches.match(e.request).then(function (response) {
-        if (response) {
-          return response;
-        } else if (e.request.headers.get("accept").includes("text/html")) {
-          return caches.match("/");
-        }
-      });
-    })
-  );
-});
+self.addEventListener('fetch', function (evt) {
+  //console.log('fetch request : ' + evt.request.url)
+  evt.respondWith(
+      caches.match(evt.request).then(function (request) {
+          if (request) { // if cache is available, respond with cache
+              //console.log('responding with cache : ' + evt.request.url)
+              return request
+          } else { // if there are no cache, try fetching request
+              console.log('file is not cached, fetching : ' + evt.request.url)
+              return fetch(evt.request)
+          }
+
+          // You can omit if/else for console.log & put one line below like this too.
+          // return request || fetch(evt.request)
+      })
+  )
+})
